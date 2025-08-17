eaa3250873e827abf2486ae9269b7cce1b8a1c1b
Marco177171 <msebastiani93@gmail.com>
2025-08-17 15:52:12 +0200

    Checking if column values are convertible into floating points

--- describe/describe.py
+++ describe/describe.py
@@ -1,9 +1,19 @@
 import csv
 
 # print numerical features
+count = 0
 with open('datasets/dataset_test.csv', 'r') as dataset: # open csv dataset
 	readable_file = csv.reader(dataset, delimiter=',')
 	for row in readable_file:
+		count += 1
+		column_index = 0
 		for column_value in row:
-			# if (isinstance(column_value, float)):
-			print(column_value)
+			column_index += 1
+			try:
+				converted = float(column_value)
+			except ValueError:
+				print(column_index, '\t: ' ,"Not a Number")
+				continue
+			print(column_index, '\t: ', converted)
+		column_index = 0
+print('Count: ', count - 1)
