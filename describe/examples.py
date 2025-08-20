faf94d07e64b4764db29def57817266bc52c223e
Marco177171 <msebastiani93@gmail.com>
2025-08-20 21:14:51 +0200

    found lowest standard deviation among tables

--- describe/examples.py
+++
@@ -1,6 +0,0 @@
-import csv
-
-with open('datasets/dataset_test.csv', newline='\n') as csvfile:
-	readable_file = csv.reader(csvfile, delimiter=' ', quotechar='|')
-	for row in readable_file:
-		print(' '.join(row))
