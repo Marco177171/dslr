0a61c09d22c2d29f72eff47037e735d011c6299f
Marco177171 <msebastiani93@gmail.com>
2025-09-19 17:35:19 +0200

    update

--- c_version/src/histogram.c
+++ c_version/src/histogram.c
@@ -28,6 +28,13 @@ int define_color(char *hogwarts_house) {
 		return(COLOR_WHITE);
 }
 
+void print_current_line(int top_lines, int start, int color_number, int len) {
+	attron(COLOR_PAIR(color_number));
+	move(top_lines + start + color_number - 1, 0);
+	hline('|', len);
+	attroff(COLOR_PAIR(color_number));
+}
+
 void visualize_data(t_data_frame*** df, int col) {
 	double min = find_min(df, col);
 	double max = find_max(df, col);
@@ -35,7 +42,6 @@ void visualize_data(t_data_frame*** df, int col) {
 	int height = (int)(max - min);
 	int top_lines = 2;
 
-	
 	initscr(); // open visualization in terminal
 	start_color(); // set color capabilities on
 	init_pair(1, COLOR_YELLOW, COLOR_BLACK); // Gryffindor
@@ -47,25 +53,41 @@ void visualize_data(t_data_frame*** df, int col) {
 	printw("%s\n", df[0][col]->s);
 
 	int i = 0, j = 0, value = 0, counter = 0, color_number = 0;
-    int len = 0, start = 0;
-    while (df[len]) len++;
+	int len = 0;
+	int start = 0;
+	int gryffindor = 0, hufflepuff = 0, ravenclaw = 0, slytherin = 0;
+	
+	while (df[len]) len++;
 	while (i < height) {
+		gryffindor = 0;
+		hufflepuff = 0;
+		ravenclaw = 0;
+		slytherin = 0;
+
 		j = 0;
-        start = i * 4;
-		counter = 0;
+		start = i * 4;
 		while (j < len) {
 			value = (int)df[j][col]->d + abs((int)min);
-			// add char at the end of line at 'i'
 			if (value == i) {
 				color_number = define_color(df[j][1]->s);
-				attron(COLOR_PAIR(color_number));
-				move(top_lines + start + house_offset(df[j][1]->s), counter);
-				addch('|');
-				attroff(COLOR_PAIR(color_number));
+				
+				if (color_number == 1)
+					gryffindor++;
+				else if (color_number == 2)
+					hufflepuff++;
+				else if (color_number == 3)
+					ravenclaw++;
+				else if (color_number == 4)
+					slytherin++;
+				
 				counter++;
 			}
 			j++;
 		}
+		print_current_line(top_lines, start, 1, gryffindor);
+		print_current_line(top_lines, start, 2, hufflepuff);
+		print_current_line(top_lines, start, 3, ravenclaw);
+		print_current_line(top_lines, start, 4, slytherin);
 		i++;
 	}
 
@@ -84,9 +106,9 @@ int main(int argc, char **argv) {
 		exit(EXIT_FAILURE);
 	}
 
-    t_data_frame*** df = get_data_frame(argv[1]);
+	t_data_frame*** df = get_data_frame(argv[1]);
 
-    visualize_data(df, atoi(argv[2]));
+	visualize_data(df, atoi(argv[2]));
 
-    free_data_frame(df);
+	free_data_frame(df);
 }
